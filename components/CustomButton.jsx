6c46a131b07ec4a68299faee28900899471d2bbc
andaru akbar <andaruakbar11@gmail.com>
2022-07-05 20:37:06 +0700

    second commit

--- components/CustomButton.jsx
+++ components/CustomButton.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 
-function CustomButton({ id, label, loading, onClick }) {
+function CustomButton({ color, radius, id, label, loading, onClick }) {
+  const mystyle = {
+    backgroundColor: color,
+    borderRadus: radius,
+  };
   return (
     <button
       id={id}
-      className={`bg-blue-700 text-white font-bold py-2 px-4 rounded-full ${
+      className={`bg-blue-700 text-white font-bold py-2 px-4 rounded-sm ${
         loading && "bg-gray-700 cursor-not-allowed"
       }`}
       onClick={onClick}
       disabled={loading}
+      style={mystyle}
     >
       {label}
     </button>
